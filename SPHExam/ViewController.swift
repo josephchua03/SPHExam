7214d1e736c141887e1337155f38edaf165ab60d
Joseph <erwin.joseph.chua@gmail.com>
2019-02-24 14:23:16 +0800

    Initial Commit

---
+++ SPHExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPHExam
+//
+//  Created by Joseph Chua on 24/2/19.
+//  Copyright © 2019 Joseph Chua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
